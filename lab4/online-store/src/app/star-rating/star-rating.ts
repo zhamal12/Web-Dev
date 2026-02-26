eb444840b23902225f331e081e66ad59e2a61a36
zhamal12 <glnrbdkrmv@gmail.com>
2026-02-26 11:39:47 +0500

    Add files via upload

---
+++ lab4/online-store/src/app/star-rating/star-rating.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-star-rating',
+  imports: [],
+  templateUrl: './star-rating.html',
+  styleUrl: './star-rating.css',
+})
+export class StarRating {
+
+}
